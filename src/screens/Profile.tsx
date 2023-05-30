afb99898de7d61c19a61a638f012cabfea6a0485
icbertoncelo <carlos.ian007@gmail.com>
2023-05-29 22:01:10 -0300

    Add expo file system and image picker

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,23 +1,65 @@
 import { useState } from 'react'
-import { TouchableOpacity } from 'react-native'
+import { Alert, TouchableOpacity } from 'react-native'
 import {
   Center,
   Heading,
   ScrollView,
   Skeleton,
   Text,
+  useToast,
   VStack,
 } from 'native-base'
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
 
 import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
 import { Button } from '@components/Button'
 
-const PHOTO_SIZE = 33
+const PHOTO_HEIGHT = 33
+const PHOTO_SIZE_IN_MB = 5
+const DEFAULT_PROFILE_URI = 'https://github.com/icbertoncelo.png'
 
 export function Profile() {
   const [isPhotoLoading, setIsPhotoLoading] = useState(false)
+  const [profileImageUri, setProfileImageUri] = useState(DEFAULT_PROFILE_URI)
+
+  const toast = useToast()
+
+  async function handlePickUserPhoto() {
+    setIsPhotoLoading(true)
+
+    try {
+      const selectedPhoto = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      })
+
+      if (!selectedPhoto.canceled) {
+        const photoUri = selectedPhoto.assets[0].uri
+
+        const photoInfo = await FileSystem.getInfoAsync(photoUri)
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > PHOTO_SIZE_IN_MB) {
+          return toast.show({
+            title: 'Imagem muito grande',
+            description: `Escolha uma imagem de até ${PHOTO_SIZE_IN_MB}Mb`,
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+        }
+
+        setProfileImageUri(photoUri)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsPhotoLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -30,8 +72,8 @@ export function Profile() {
         <Center mt={6} px={10}>
           {isPhotoLoading ? (
             <Skeleton
-              w={PHOTO_SIZE}
-              h={PHOTO_SIZE}
+              w={PHOTO_HEIGHT}
+              h={PHOTO_HEIGHT}
               rounded="full"
               startColor="gray.500"
               endColor="gray.400"
@@ -39,14 +81,14 @@ export function Profile() {
           ) : (
             <UserPhoto
               source={{
-                uri: 'https://github.com/icbertoncelo.png',
+                uri: profileImageUri,
               }}
               alt="Foto do usuário"
-              size={PHOTO_SIZE}
+              size={PHOTO_HEIGHT}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handlePickUserPhoto}>
             <Text
               color="green.500"
               fontWeight="bold"
